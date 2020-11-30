95714e35d89d66fcf1d2a7ea1f84398dd53f19ee
duongdev <=>
2020-11-30 17:11:57 +0700

    Show categories room

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root to: "static_pages#home"
+
     get "/categories/:slug/rooms", to: "rooms#index"
     get "/room/:slug", to: "rooms#show", as: "room"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     get "/search", to: "search#index"
+    get "categories", to: "categories#index"
   end
 end
